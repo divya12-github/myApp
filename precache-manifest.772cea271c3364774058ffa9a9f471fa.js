fcd70ba2cee9b45910f5b395f7850b00135c4a5f
unknown <sahudivya946@gmail.com>
2021-01-28 13:04:55 +0530

    Updates

---
+++ precache-manifest.772cea271c3364774058ffa9a9f471fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee76dc72dd48bb64b1b91f5f90f7458",
+    "url": "/myApp/index.html"
+  },
+  {
+    "revision": "08af80c63a4f4c5c49db",
+    "url": "/myApp/static/css/2.e09ef840.chunk.css"
+  },
+  {
+    "revision": "2d8e71550d4b3908dae6",
+    "url": "/myApp/static/css/main.cd44a3b9.chunk.css"
+  },
+  {
+    "revision": "08af80c63a4f4c5c49db",
+    "url": "/myApp/static/js/2.75777eb7.chunk.js"
+  },
+  {
+    "revision": "9e878caba7b1f08e0041eca808a449aa",
+    "url": "/myApp/static/js/2.75777eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d8e71550d4b3908dae6",
+    "url": "/myApp/static/js/main.1acb86ab.chunk.js"
+  },
+  {
+    "revision": "0dbdb1e892e5c812891e",
+    "url": "/myApp/static/js/runtime-main.a8b6235a.js"
+  }
+]);
